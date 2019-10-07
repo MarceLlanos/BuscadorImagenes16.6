95826152e175a92b989f2402d89ba55c1eb7cfe1
Marcela Llanos <marcela3llanos@gmail.com>
2019-10-07 16:27:50 -0400

    Se termino todo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Buscador from './components/Buscador';
+import Resultado from './components/Resultado';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = { 
+    termino: '',
+    imagenes: [],
+    pagina: ''
+  }
+
+  consultarApi = () => {
+    const termino = this.state.termino;
+    const pagina = this.state.pagina;
+
+    const url = `https://pixabay.com/api/?key=13868013-0658ade3a0477a9cff0c75d2e&q=${termino}&per_page=30&page=${pagina}`;
+    console.log(url);
+
+    fetch(url)
+      .then(respuesta => respuesta.json())
+      .then(resultado => this.setState({imagenes: resultado.hits}))
+
+  }
+
+  datosBusqueda = (termino) => {
+    this.setState({
+      termino: termino,
+      pagina: 1
+    }, () => {
+      this.consultarApi();
+    })
+  }
+
+  paginaAnterior = () => {
+    //leemos el state
+    let pagina = this.state.pagina;
+
+    // si es la pagina 1 ya no se puede retroceder
+    if(pagina === 1) return null;
+
+    //restar a la pagina actual
+    pagina -=1;
+
+    // agregar al state
+    this.setState({
+      pagina
+    }, () => {
+      this.consultarApi();
+      this.scroll();
+    })
+  }
+
+  paginaSiguiente = () => {
+    // leemos el state
+    let pagina = this.state.pagina;
+
+    // sumar a la pagina actual
+    pagina +=1;
+
+    //agregamos al state
+    this.setState({
+      pagina
+    }, () => {
+      this.consultarApi();
+      this.scroll();
+    })
+  }
+
+  scroll = () => {
+    const elemento = document.querySelector('#resultado');
+    elemento.scrollIntoView('auto', 'start');
+  }
+
+  render() { 
+    return ( 
+      <div className="app container justify-content-center">
+        <div className="jumbotron">
+          <Header title="Buscador de Imagenes"/>
+          <Buscador
+            datosBusqueda={this.datosBusqueda}
+          />
+        </div>
+        <div className="row">
+          <Resultado
+            imagenes = {this.state.imagenes}
+            paginaAnterior={this.paginaAnterior}
+            paginaSiguiente={this.paginaSiguiente}
+          />
+        </div>
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = ({title}) => {
+  return (
+    <p className="lead text-center">
+      {title}
+    </p>
+  );
+}
+ 
+export default Header;
